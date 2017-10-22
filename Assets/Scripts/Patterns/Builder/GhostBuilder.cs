05a090b3f4b79317a93b1e950e5b8aafceb59baa
Martynas Kanapinskas <Martynas.Kanapinskas>
2017-10-22 12:35:09 +0300

    Added logic for moving ghosts

--- Assets/Scripts/Patterns/Builder/GhostBuilder.cs
+++ Assets/Scripts/Patterns/Builder/GhostBuilder.cs
@@ -12,9 +12,17 @@ namespace Assets.Scripts.Patterns.Builder
         {
             gameObject = GameObject.CreatePrimitive(PrimitiveType.Sphere);
 
+            gameObject.transform.position = new Vector3(0, -10, 0);
             gameObject.AddComponent<GhostComponent>().InjectAttributes(AssembleGhostComponent());
-            gameObject.AddComponent<MeshCollider>();
             gameObject.AddComponent<CharacterController>();
+            gameObject.AddComponent<Rigidbody>();
+
+            var renderer = gameObject.GetComponent<Renderer>();
+            renderer.material = Resources.Load<Material>("Blue");
+
+            var collider = gameObject.GetComponent<SphereCollider>();
+            collider.isTrigger = true;
+            collider.radius = 0.6f;
 
             gameObject.name = "Ghost";
             return gameObject;
@@ -28,9 +36,17 @@ namespace Assets.Scripts.Patterns.Builder
                 Body = new SmallBody(),
                 Mouth = new CharacterMouth("Boo, I'm a ghost!")
             };
-            ghost.Position = new Vector3(50f, ghost.Body.Height / 2f, 50);
+            ghost.Position = GetRandomLocation(ghost.Body.Height);
 
             return ghost;
+        }
+
+        private Vector3 GetRandomLocation(float yAxis)
+        {
+            float xAxis = Random.value > 0.5 ? 35f : 65f;
+            float zAxis = Random.value * 35 + 30;
+
+            return new Vector3(xAxis, yAxis, zAxis);
         }
     }
 }
