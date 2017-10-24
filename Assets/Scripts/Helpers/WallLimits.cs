4398992872d2743e8fbcb28688ab9448ddb14229
Martynas Kanapinskas <Martynas.Kanapinskas>
2017-10-24 22:56:49 +0300

    Added some minimal refactoring for solution.

---
+++ Assets/Scripts/Helpers/WallLimits.cs
@@ -0,0 +1,10 @@
+﻿namespace Assets.Scripts.Helpers
+{
+    public class WallLimits
+    {
+        public float FromX;
+        public float ToX;
+        public float FromZ;
+        public float ToZ;
+    }
+}
