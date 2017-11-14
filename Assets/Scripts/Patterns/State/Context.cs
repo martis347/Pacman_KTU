8e6f3e9d35f37050bfb1e2d8df9e067bd92e58c9
Tomuuunis <tomuuunis@gmail.com>
2017-11-14 23:18:49 +0200

    added state pattern

---
+++ Assets/Scripts/Patterns/State/Context.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Assets.Scripts.Patterns.State
+{
+    class Context
+    {
+        private IState state;
+
+        public Context()
+        {
+            state = null;
+        }
+
+        public void setState(IState newState)
+        {
+            state = newState;
+        }
+
+        public IState getState()
+        {
+            return state;
+        }
+    }
+}
