b6c71aedffe9789630fc4896290c541f552c56c1
Tomuuunis <tomuuunis@gmail.com>
2017-11-26 15:34:17 +0200

    Memento pattern (#6)
    * initialized memento pattern
    
    * implemented memento pattern

---
+++ Assets/Scripts/Patterns/Memento/Memento.cs
@@ -0,0 +1,24 @@
+﻿using Assets.Scripts.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace Assets.Scripts.Patterns.Memento
+{
+    class Memento
+    {
+        private PacmanComponent pacmanState;
+
+        public Memento(PacmanComponent pacmanState)
+        {
+            this.pacmanState = pacmanState;
+        }
+
+        public PacmanComponent getPacmanState()
+        {
+            return pacmanState;
+        }
+    }
+}
---
+++ Assets/Scripts/Patterns/Memento/Originator.cs
@@ -0,0 +1,34 @@
+﻿using Assets.Scripts.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace Assets.Scripts.Patterns.Memento
+{
+    class Originator
+    {
+        private PacmanComponent pacmanState;
+
+        public void setState(PacmanComponent pacmanState)
+        {
+            this.pacmanState = pacmanState;
+        }
+
+        public PacmanComponent getState()
+        {
+            return pacmanState;
+        }
+
+        public Memento saveStateToMemento()
+        {
+            return new Memento(pacmanState);
+        }
+
+        public void getStateFromMemento(Memento memento)
+        {
+            pacmanState = memento.getPacmanState();
+        }
+    }
+}
